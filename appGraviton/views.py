f34b2e283f96d85a20a70e5f760221443accab59
caeltarifa <caeltarifa@gmail.com>
2020-01-16 07:37:41 -0400

    mostrando linea de tiempo de datos en index.html

--- appGraviton/views.py
+++ appGraviton/views.py
@@ -16,9 +16,9 @@ def principal_presentacion(request):
 
 def despues_login(request):
     if request.user.is_authenticated:
-        metar = Metar_trafico.objects.raw("select * from plan_vuelo_flp_trafico where hora_amhs like '12%%%%' order by id_amhs desc limit 20")
-        return render(request, 'index.html', {'metar': metar})
-        #return render(request, 'index.html')
+        #metar = Metar_trafico.objects.raw("select * from plan_vuelo_flp_trafico where hora_amhs like '12%%%%' order by id_amhs desc limit 20")
+        #return render(request, 'index.html', {'metar': metar})
+        return render(request, 'index.html')
     else:
         return redirect('login')
 
